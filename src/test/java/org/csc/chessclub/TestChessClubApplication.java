72458ce8ffaff36bcf728b603c65e97ec83225ca
FabioFlo <fabio.floris@outlook.it>
2025-04-29 19:43:48 +0200

    first commit

---
+++ src/test/java/org/csc/chessclub/TestChessClubApplication.java
@@ -0,0 +1,11 @@
+package org.csc.chessclub;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestChessClubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(ChessClubApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
