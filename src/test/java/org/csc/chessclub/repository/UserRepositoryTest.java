c824a01af87714384f4490f83bcf02b18f17f6d2
FabioFlo <fabio.floris@outlook.it>
2025-05-20 11:30:01 +0200

    feat: user repository unit test, find user with username or email but different uuid

--- src/test/java/org/csc/chessclub/repository/UserRepositoryTest.java
+++ src/test/java/org/csc/chessclub/repository/UserRepositoryTest.java
@@ -3,6 +3,7 @@ package org.csc.chessclub.repository;
 import org.csc.chessclub.enums.Role;
 import org.csc.chessclub.model.UserEntity;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -47,12 +48,26 @@ public class UserRepositoryTest {
     }
 
     @Test
-    void testFindUserByEmailOrUsername_whenGivenUsernameOrEmail_returnUserWithGivenUsernameOrEmail() {
+    @DisplayName("Find user by username or email")
+    void testFindUserByUsernameOrEmail_whenGivenUsernameOrEmail_returnUserWithGivenUsernameOrEmail() {
         userRepository.save(user);
         Optional<UserEntity> retrievedUser = userRepository.findUserEntityByUsernameOrEmail(USERNAME, EMAIL);
 
         assertTrue(retrievedUser.isPresent(), "User should be present");
         assertEquals(USERNAME, retrievedUser.get().getUsername(), "Username should be equal");
         assertEquals(EMAIL, retrievedUser.get().getEmail(), "Email should be equal");
+    }
+
+    @Test
+    @DisplayName("Find user by username or email with different uuid")
+    void testFindUserUsernameOrEmailWithDifferentUuid_whenGivenUsernameOrEmailAndUuidNot_returnUserWithGivenUsernameOrEmailAndIdNot() {
+        userRepository.save(user);
+        Optional<UserEntity> retrievedUser = userRepository.findByUsernameOrEmailAndUuidNot(USERNAME, EMAIL, user.getUuid());
+
+        assertTrue(retrievedUser.isPresent(), "User should be present");
+        assertEquals(USERNAME, retrievedUser.get().getUsername(),
+                "Username should be equal");
+        assertEquals(EMAIL, retrievedUser.get().getEmail(),
+                "Email should be equal");
     }
 }
