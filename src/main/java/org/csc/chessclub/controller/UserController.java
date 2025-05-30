f724f4a83afba106c901ab5937e015b1261b3c34
FabioFlo <fabio.floris@outlook.it>
2025-05-30 11:03:24 +0200

    refactor: PreAuthorize in update user

--- src/main/java/org/csc/chessclub/controller/UserController.java
+++ src/main/java/org/csc/chessclub/controller/UserController.java
@@ -40,7 +40,7 @@ public class UserController {
                 .body(new ResponseDto<>(authService.authenticate(request), "User logged in", true));
     }
 
-    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
+    @PreAuthorize("isAuthenticated")
     @PatchMapping()
     public ResponseEntity<ResponseDto<UpdateUserRequest>> updateUser(@Valid @RequestBody UpdateUserRequest userRequest) {
         userService.update(userMapper.updateUserRequestToUser(userRequest));
